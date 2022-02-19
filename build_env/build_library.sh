38d179bbf223e145d1e2d6f4268cfe6a3cafcd73
imotai <codego.me@gmail.com>
2022-02-19 18:06:54 +0800

    fix: move absl build to the end

--- build_env/build_library.sh
+++ build_env/build_library.sh
@@ -144,20 +144,7 @@ else
 	echo "install gflags done"
 fi
 
-if [ ! -f "absl_succ" ]; then
-	echo "installing gtest ...."
-	tar xzf a50ae369a30f99f79d7559002aba3413dac1bd48.tar.gz
-	pushd abseil-cpp-a50ae369a30f99f79d7559002aba3413dac1bd48
-	cmake -DCMAKE_INSTALL_PREFIX="$DEPS_PREFIX" -DCMAKE_CXX_FLAGS=-fPIC -DABSL_ENABLE_INSTALL=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=17 -DABSL_USE_GOOGLETEST_HEAD=OFF  -DCMAKE_POSITION_INDEPENDENT_CODE=ON 
-	make "-j$(nproc)"
-	make install
-    ls -l ${DEPS_PREFIX}/lib64 | grep  flags
-    cp absl/flags/libabsl_flags_private_handle_accessor.a ${DEPS_PREFIX}/lib64/
-    ls -l ${DEPS_PREFIX}/lib64 | grep  flags
-	popd
-	touch absl_succ
-	echo "install absl done"
-fi
+
 
 if [ -f "unwind_succ" ]; then
 	echo "unwind_exist"
@@ -347,11 +334,19 @@ else
     echo "install boost done"
 fi
 
-ls -l ${DEPS_PREFIX}/lib64 | grep  flags
-
-# Remove dynamic library files for static link
 find /depends/thirdparty/lib/ -name "lib*so*" | grep -v "libRemarks" | grep -v "libLTO" | xargs rm
 find /depends/thirdparty/lib64/ -name "lib*so*" | grep -v "libRemarks" | grep -v "libLTO" | xargs rm
-ls -l ${DEPS_PREFIX}/lib64 | grep  flags
+
+if [ ! -f "absl_succ" ]; then
+	echo "installing gtest ...."
+	tar xzf a50ae369a30f99f79d7559002aba3413dac1bd48.tar.gz
+	pushd abseil-cpp-a50ae369a30f99f79d7559002aba3413dac1bd48
+	cmake -DCMAKE_INSTALL_PREFIX="$DEPS_PREFIX" -DCMAKE_CXX_FLAGS=-fPIC -DABSL_ENABLE_INSTALL=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=17 -DABSL_USE_GOOGLETEST_HEAD=OFF  -DCMAKE_POSITION_INDEPENDENT_CODE=ON 
+	make "-j$(nproc)"
+	make install
+	popd
+	touch absl_succ
+	echo "install absl done"
+fi
 
 popd
