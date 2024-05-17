9a31e6222fbe81208386ad6eaeb15920d7cca41f
imotai <codego.me@gmail.com>
2024-05-17 16:22:39 +0800

    feat: add setup for github copilot

--- set_vim.sh
+++ set_vim.sh
@@ -3,4 +3,11 @@ mv ~/.vim ~/.vim.orig
 mv ~/.vimrc ~/.vimrc.orig
 cp -rf vim ~/.vim
 ln -s ~/.vim/vimrc ~/.vimrc
-echo "config vim ok , please run PluginInstal in vim!"
+echo "config plugin done"
+
+git clone git@github.com:github/copilot.vim.git \
+~/.vim/pack/github/start/copilot.vim
+
+echo "download github copilot done"
+
+
